4f3b1a5ee7b48cc2b04e4500c705d0e2db55caae
nshportun <nshportun@example.com>
2026-06-23 22:04:19 -0600

    chore: rename to scoped package @nshportun/eslint-plugin-safe-async

--- src/utils/docs-url.ts
+++ src/utils/docs-url.ts
@@ -1,4 +1,4 @@
-const DOCS_BASE_URL = 'https://github.com/nshportun/eslint-plugin-safe-async/blob/main/docs/rules';
+const DOCS_BASE_URL = 'https://github.com/nshportun/eslint-plugin-safe-async/blob/master/docs/rules';
 
 export function docsUrl(ruleName: string): string {
   return `${DOCS_BASE_URL}/${ruleName}.md`;
